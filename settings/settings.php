7a8db6bd50d0c291cca6e4a0bba8c944cc479dba
Trewep <56358560+Trewep@users.noreply.github.com>
2021-03-22 10:21:51 +0100

    add db connection and settingsfile

---
+++ settings/settings.php
@@ -0,0 +1,10 @@
+<?php   
+
+        const SETTINGS = [
+                "db" =>[
+                        "user"                  =>"bbchaacht_bethomasmore_php",
+                        "password"      =>"9w7kws81nVZkbBdzrn89",
+                        "host"                  =>"bbchaacht.be.mysql.service.one.com",
+                        "dbname"          =>"bbchaacht_bethomasmore_php"
+                ]
+                ];
