a9c7d766adad8014bc2ad60f1800615e8862d05d
HungryHypnoHippo <plyusninilya97@gmail.com>
2021-04-23 16:34:14 +0200

    Foto posten, logout en tagOverview

--- tagOverview.php
+++ tagOverview.php
@@ -1,3 +1,7 @@
+<?php
+    var_dump($_GET);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +19,25 @@
     <!--design pagina: https://www.figma.com/proto/jzjm99ggCTUSNv7ITLuLZl/PHP-project-DEBUFF?node-id=3%3A170&viewport=444%2C-1081%2C0.47289735078811646&scaling=scale-down-->
 </head>
 <body>
-    
+    <div id="posts">
+        <?php
+            //db connectie om alle posts op te halen die aan de aangeklikte tag voldoen
+            $conn = new PDO('mysql:host=localhost;dbname=debuff', 'root', 'root');
+            $statement = $conn->prepare("SELECT * FROM posts WHERE tags = :tags ORDER BY id DESC");
+            $statement->bindValue(":tags", $_GET['tag']);
+            $statement->execute();
+            $posts = $statement->fetchAll();
+        ?>
+        <?php foreach ($posts as $post): ?>
+            <div class="post">
+                <img id="postImage" src= <?php echo "assets/testImages/" . $post['image'] ?> alt="">
+                <p id="postDescription"><?php echo htmlspecialchars($post['description']) ?></p>
+                <p id="postLocation"><?php echo htmlspecialchars($post['location']) ?></p>
+                <!--Deze tag redirect naar een overview pagina waar alleen posts staan die deze tag hebben-->
+                <a id="postTags" href="tagOverview.php?tag=<?php echo htmlspecialchars($post['tags']) ?>"><?php echo "#" . htmlspecialchars($post['tags']) ?></a>
+                <p><?php echo $post['postTime'] ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
